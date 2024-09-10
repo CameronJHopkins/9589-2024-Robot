604a6475b03c49a3751f0771bd3bf7f2cd5a77ce
CJ Hopkins <cameronh344728@gmail.com>
2024-09-10 12:37:29 -0400

    Uncommented Newmatics Functions

--- src/main/java/frc/robot/Subsystems/Newmatics.java
+++ src/main/java/frc/robot/Subsystems/Newmatics.java
@@ -2,22 +2,22 @@ package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import edu.wpi.first.wpilibj.Compressor;
-// import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class Newmatics extends SubsystemBase {
 
-    // static Compressor Pshhhhhhh = new Compressor(PneumaticsModuleType.REVPH);
+    static Compressor Pshhhhhhh = new Compressor(PneumaticsModuleType.REVPH);
     
     public void Arise() {
 
-        // Pshhhhhhh.enableAnalog(70, 120);
+        Pshhhhhhh.enableAnalog(70, 120);
 	
     }
     
     public void nopshhh(){
 
-        // Pshhhhhhh.disable();
+        Pshhhhhhh.disable();
 
     }
 }
